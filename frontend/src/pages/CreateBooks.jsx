630fdbd81b756426b0688a60751cfd460a80cd62
gitcommitcode <rahulkmr5041@gmail.com>
2023-11-10 22:38:08 +0530

    crud completed

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -33,6 +33,8 @@ const CreateBooks = () => {
       <h1 className="text-3xl my-4">Create Books</h1>
       {loading?
         <Spinner/>:''}
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto' >
+
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
           <input 
@@ -40,7 +42,7 @@ const CreateBooks = () => {
             value={title}
             onChange={(e)=>setTitle(e.target.value)}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
+            />
         </div>
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Author</label>
@@ -49,7 +51,7 @@ const CreateBooks = () => {
             value={author}
             onChange={(e)=>setAuthor(e.target.value)}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
+            />
         </div>
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">PublishYear</label>
@@ -58,9 +60,10 @@ const CreateBooks = () => {
             value={publishYear}
             onChange={(e)=>setPublishYear(e.target.value)}
             className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
+            />
         </div>
         <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>Save</button>
+        </div>
     </div>
   )
 }
